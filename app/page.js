23f2d59e5ef896cc3b79d1425fe13d56132b8dc0
Kevin Yang <kev4238@gmail.com>
2024-06-27 11:19:48 -0400

    Added initial website commit (June 26, 2024)

--- app/page.js
+++ app/page.js
@@ -1,113 +1,80 @@
-import Image from "next/image";
+"use client";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import {
+  AiFillLinkedin,
+  AiFillGithub,
+  AiFillGoogleCircle
+} from "react-icons/ai";
+import { useState } from "react";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(true);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className={darkMode ? "dark" : ""}>
+      <main className="bg-white px-10 flex justify-center dark:bg-black bg-[radial-gradient(#e5e7eb_0px,transparent_1px)] [background-size:50px_50px]">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl dark:text-white">K. Y.</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill
+                  className="cursor-pointer text-2xl dark:text-white"
+                  onClick={() => setDarkMode(!darkMode)}
+                ></BsFillMoonStarsFill>
+              </li>
+              <li>
+                <button
+                  className="bg-gradient-to-r from-purple-400 to-purple-200 text-white px-4 py-2 rounded-md ml-8 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-250"
+                  href="#"
+                >
+                  Resume
+                </button>
+              </li>
+            </ul>
+          </nav>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <div className="text-center p-10">
+            <h2 className="text-6xl text-left py-2 font-medium dark:text-white">
+              Hey, I'm Kevin
+            </h2>
+            <h3 className="text-4xl pt-1 pb-6 text-left bg-gradient-to-r from-purple-600 to-purple-200 bg-clip-text text-transparent font-medium">
+              Developer and photographer.
+            </h3>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <div className="flex justify-left gap-5">
+              <button className="text-2xl py-6 flex justify-left gap-5 text-gray-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-250 dark:text-white">
+                <div className="flex justify-normal gap-2 bg-gray-700 rounded-3xl px-5 py-3">
+                  <AiFillLinkedin></AiFillLinkedin>
+                  <h1 className="text-sm">LinkedIn</h1>
+                </div>
+              </button>
+              <button className="text-2xl py-6 flex justify-left gap-5 text-gray-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-250 dark:text-white">
+                <div className="flex justify-normal gap-2 bg-gray-700 rounded-3xl px-5 py-3">
+                  <AiFillGithub></AiFillGithub>
+                  <h1 className="text-sm">Github</h1>
+                </div>
+              </button>
+              <button className="text-2xl py-6 flex justify-left gap-5 text-gray-700 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-250 dark:text-white rounded-full">
+                <div className="flex justify-normal gap-2 bg-gray-700 rounded-3xl px-5 py-3">
+                  <AiFillGoogleCircle></AiFillGoogleCircle>
+                  <h1 className="text-sm">Gmail</h1>
+                </div>
+              </button>
+            </div>
+            <p className="text-left py-3 leading-8 text-gray-800 max-w-xl dark:text-white">
+              Hi I am Kevin, a software developer from Canada. I have 2 years of
+              experience in Java automation, Serenity, and Selenium for
+              microservice, UI, and SQL testing. Let's work together!
+            </p>
+          </div>
+        </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <section className="min-h-screen">
+          <div>
+            <hr className="h-px my-8 bg-white border-0 dark:bg-white"></hr>
+          </div>
+        </section>
+      </main>
+    </div>
   );
 }
